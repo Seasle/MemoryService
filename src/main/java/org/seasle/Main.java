f806d33913b03842f1cd784d62199d0cdb91f185
Seasle <seasle98@gmail.com>
2020-06-07 15:43:14 +0600

    Добавление блокировки экземпляра программы

--- src/main/java/org/seasle/Main.java
+++ src/main/java/org/seasle/Main.java
@@ -1,26 +1,70 @@
 package org.seasle;
 
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.channels.FileLock;
 import java.util.Set;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class Main {
-    public static Database database = new Database("database");
+    public static Logger logger = LoggerProvider.getInstance();
+    public static Database database = null;
 
     public static void main(String[] args) {
-        Statistics statistics = new Statistics();
+        if (lockInstance("MemoryService")) {
+            database = new Database("database");
 
-        Set<String> disks = statistics.getDisks();
-        for (String disk : disks) {
-            database.putData(statistics.getDiskInfo(disk));
+            Statistics statistics = new Statistics();
+
+            Set<String> disks = statistics.getDisks();
+            for (String disk : disks) {
+                database.putData(statistics.getDiskInfo(disk));
+            }
+
+            FormManager formManager = FormManager.getInstance();
+
+            formManager.setDatabase(database);
+            formManager.setDisks(disks);
+            formManager.showInterface();
+
+            Runtime.getRuntime().addShutdownHook(new Thread(Main::exit));
+        } else {
+            System.exit(0);
         }
-
-        FormManager formManager = FormManager.getInstance();
-
-        formManager.setDatabase(database);
-        formManager.setDisks(disks);
-        formManager.showInterface();
     }
 
-    public static void exit(int status) {
+    private static boolean lockInstance(final String lockFile) {
+        try {
+            final File file = new File(lockFile);
+            final RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+            final FileLock fileLock = randomAccessFile.getChannel().tryLock();
+
+            if (fileLock != null) {
+                Files.setAttribute(file.toPath(), "dos:hidden", true, LinkOption.NOFOLLOW_LINKS);
+
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    try {
+                        fileLock.release();
+                        randomAccessFile.close();
+                        file.delete();
+                    } catch (Exception exception) {
+                        logger.log(Level.SEVERE, String.format("Unable to remove lock file: %s", lockFile));
+                    }
+                }));
+
+                return true;
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, String.format("Unable to create and/or lock file: %s", lockFile));
+        }
+
+        return false;
+    }
+
+    private static void exit() {
         database.close();
     }
 }
